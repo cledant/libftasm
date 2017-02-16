e469f3cabaa6064ba05f7b4effa51908fb165dec
cledant <cledant@student.42.fr>
2017-02-16 16:21:40 +0100

    added ft_puts

--- srcs/ft_puts.s
+++ srcs/ft_puts.s
@@ -2,17 +2,63 @@
 %define STDOUT				1
 %define WRITE				4
 
+section .data
+text_null:
+	.string db "(null)", 10
+	.len equ $ - text_null.string
+new_line:
+	.string db "", 10
+	.len equ $ - new_line.string
+
+
 section .text
 	global _ft_puts
 	extern _ft_strlen
 
 _ft_puts:
-	call _ft_strlen
-	mov rsi, rdi
+	push rbp							;Set ret
+	mov rbp, rsp
+	cmp rdi, 0							;Case for s = NULL
+	je _disp_null
+	push r12							;Save s addr in r12
+	mov r12, rdi
+	call _ft_strlen						;Getting s len
+	mov rsi, r12						;Setting call to write string
 	mov rdi, STDOUT
 	mov rdx, rax
 	mov rax, MACH_SYSCALL(WRITE)
 	syscall
-	leave
-	mov rax, 1
+	cmp rax, 0							;Checking if write failed
+	jl _fail
+	lea rsi, [rel new_line.string]		;Setting call to write new_line
+	mov rdi, STDOUT
+	mov rdx, new_line.len
+	mov rax, MACH_SYSCALL(WRITE)
+	syscall
+	cmp rax, 0							;Checking if write failed
+	jl _fail
+	mov rax, 10
+	pop r12								;Restoring values
+	pop rbp
+	ret
+
+_fail:
+	pop r12								;Restoring values
+	pop rbp
+	ret
+
+_disp_null:
+	lea rsi, [rel text_null.string]		;Setting call to write
+	mov rdi, STDOUT
+	mov rdx, text_null.len
+	mov rax, MACH_SYSCALL(WRITE)
+	syscall
+	cmp rax, 0							;Checking if write failed
+	jl _fail_null
+	mov rax, 10
+	pop rbp								;Restoring values
+	ret
+
+_fail_null:
+	pop rbp								;Restoring values
 	ret
--- srcs/unit_test/test_puts.c
+++ srcs/unit_test/test_puts.c
@@ -6,7 +6,7 @@
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/14 16:40:29 by cledant           #+#    #+#             */
-/*   Updated: 2017/02/15 13:34:49 by cledant          ###   ########.fr       */
+/*   Updated: 2017/02/16 16:20:52 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,24 @@ int		ft_puts(const char *s);
 
 int		main(void)
 {
-	char	*test = NULL;
+	char	*test = "caca";
+	char	*test2 = NULL;
 	int		ret;
 
-	printf("%s \n", test);
-	printf("=================\n");
-//	ret = ft_puts(test);
-	printf("=================\n");
-	printf("Return value moi: %d\n", ret);
+	printf("EOF value %d\n", EOF);
 	printf("=================\n");
 	ret = puts(test);
+	printf("Return value vrai: %d\n", ret);
 	printf("=================\n");
-	printf("\nReturn value vrai: %d\n", ret);
+	ret = ft_puts(test);
+	printf("Return value moi: %d\n", ret);
+	printf("=================\n");
+	printf("=================\n");
+	ret = puts(test2);
+	printf("Return value vrai: %d\n", ret);
+	printf("=================\n");
+	ret = ft_puts(test2);
+	printf("Return value moi: %d\n", ret);
+	printf("=================\n");
 	return (0);
 }
